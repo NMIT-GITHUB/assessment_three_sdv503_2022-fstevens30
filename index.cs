41adb560265630ce9eb239c0bae6dfe635c681e0
fstevens30 <flynn-stevens@live.nmit.ac.nz>
2022-06-02 14:52:08 +1200

    Added initial array sorting

---
+++ index.cs
@@ -0,0 +1,24 @@
+int[][] exampleJavaScript =
+{
+    new int[]
+    {
+        4, 2, 7, 1
+    },
+    new int[]
+    {
+        20, 70, 40, 90
+    },
+    new int[]
+    {
+        1, 2, 0
+    }
+};
+
+for (int i = 0; i < exampleJavaScript.Length; i++)
+{
+    for (int j = 0; j < exampleJavaScript[i].Length; j++)
+    {
+        Console.WriteLine(exampleJavaScript[i][j]);
+    }
+};
+Console.WriteLine("---------------------------------------------");
