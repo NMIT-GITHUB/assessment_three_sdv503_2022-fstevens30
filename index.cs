8a43baa212e799504ec42c8927dd6f4bfbfe1bee
fstevens30 <flynn-stevens@live.nmit.ac.nz>
2022-06-02 15:26:36 +1200

    Added more to for loops to get largest int

--- index.cs
+++ index.cs
@@ -1,24 +1,16 @@
-int[][] exampleJavaScript =
-{
-    new int[]
-    {
-        4, 2, 7, 1
-    },
-    new int[]
-    {
-        20, 70, 40, 90
-    },
-    new int[]
-    {
-        1, 2, 0
-    }
-};
+int[][] exampleJavaScript = new int[3][];
+exampleJavaScript[0] = new int[] { 4, 2, 7, 1 };
+exampleJavaScript[1] = new int[] { 20, 70, 40, 90 };
+exampleJavaScript[2] = new int[] { 1, 2, 0 };
 
 for (int i = 0; i < exampleJavaScript.Length; i++)
 {
+    int largest = exampleJavaScript[i][0];
     for (int j = 0; j < exampleJavaScript[i].Length; j++)
     {
-        Console.WriteLine(exampleJavaScript[i][j]);
+        if (exampleJavaScript[i][j] > largest)
+        {
+            largest = exampleJavaScript[i][j];
+        }
     }
 };
-Console.WriteLine("---------------------------------------------");
