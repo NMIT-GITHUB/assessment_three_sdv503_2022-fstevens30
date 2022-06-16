2798cd58d8cf2640b29e6b86b5dc4ced5f772d56
fstevens30 <flynn-stevens@live.nmit.ac.nz>
2022-06-16 12:00:01 +1200

    Changed variable names in index.cs

--- index.cs
+++ index.cs
@@ -1,16 +1,19 @@
-int[][] exampleJavaScript = new int[3][];
-exampleJavaScript[0] = new int[] { 4, 2, 7, 1 };
-exampleJavaScript[1] = new int[] { 20, 70, 40, 90 };
-exampleJavaScript[2] = new int[] { 1, 2, 0 };
+int[][] arr = new int[3][];
+arr[0] = new int[] { 4, 2, 7, 1 };
+arr[1] = new int[] { 20, 70, 40, 90 };
+arr[2] = new int[] { 1, 2, 0 };
 
-for (int i = 0; i < exampleJavaScript.Length; i++)
+
+for (int i = 0; i < arr.Length; i++)
 {
-    int largest = exampleJavaScript[i][0];
-    for (int j = 0; j < exampleJavaScript[i].Length; j++)
+    int largest = arr[i][0];
+    for (int j = 1; j < arr[i].Length; j++)
     {
-        if (exampleJavaScript[i][j] > largest)
+        if (arr[i][j] > largest)
         {
-            largest = exampleJavaScript[i][j];
+            largest = arr[i][j];
+            Console.WriteLine(largest);
         }
     }
-};
+}
+
